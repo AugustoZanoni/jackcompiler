29bda5dd1afdbd8c4c8467efd1dd3f56d9531206
AugustoZanoni <49280362+AugustoZanoni@users.noreply.github.com>
2021-03-07 23:40:43 -0300

    JAckAnalyzer
    Start developing jack analyzer, class and let statement

---
+++ JackCompiler/CompilationEngine.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace JackCompiler
+{
+    class CompilationEngine
+    {
+        static protected XmlWriterSettings settings = new XmlWriterSettings()
+        {
+            Indent = true,
+            IndentChars = "    "
+        };
+        public XmlWriter writer = XmlWriter.Create(Console.Out, settings);
+        public CompilationEngine(string path)
+        {
+            try
+            {
+                JackTokenizer Tokenizer = new JackTokenizer(path);
+                Tokenizer.advance();
+                CompileClass(Tokenizer);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public void CompileClass(JackTokenizer tokenizer)
+        {
+            if (tokenizer.token().content.ToLower() == "class")
+            {
+                writer.WriteStartElement("class");
+                writer.WriteElementString(tokenizer.token().type.ToString(), tokenizer.token().content);
+                tokenizer.advance();
+                if(tokenizer.token().type == JackTokenizer.Token.Type.IDENTIFIER)
+                    writer.WriteElementString(tokenizer.token().type.ToString(), tokenizer.token().content);
+                else
+                    throw new CompilerException();
+                tokenizer.advance();
+                if(tokenizer.token().type == JackTokenizer.Token.Type.SYMBOL)
+                    writer.WriteElementString(tokenizer.token().type.ToString(), tokenizer.token().content);
+                else
+                    throw new CompilerException();
+
+                while (tokenizer.hasMoreTokens())
+                {
+                    tokenizer.advance();
+                    if (tokenizer.token().type == JackTokenizer.Token.Type.KEYWORD)
+                    {
+                        switch (tokenizer.token().content.ToLower())
+                        {
+                            case "static":
+                                CompileClassVarDec(tokenizer);
+                                break;
+                            case "field":
+                                CompileClassVarDec(tokenizer);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    //else 
+                    //    throw new CompilerException();
+                }
+
+                //Corrigir
+                writer.WriteElementString("SYMBOL", "}");
+
+                writer.WriteEndElement();
+                writer.Flush();
+            } else {
+                throw new CompilerException();
+            }
+        }
+
+        public void CompileClassVarDec(JackTokenizer tokenizer)
+        {
+            XElement classVarDec = new XElement("classVarDec");
+            if(tokenizer.token().type == JackTokenizer.Token.Type.KEYWORD && Regex.IsMatch( tokenizer.token().content.ToLower(), "(static|field)"))
+                classVarDec.Add(new XElement(tokenizer.token().type.ToString(), tokenizer.token().content));
+            else
+                throw new CompilerException();
+            tokenizer.advance();
+            if(tokenizer.token().type == JackTokenizer.Token.Type.KEYWORD) //Verificar Possibilidade de ClassName (Não COntempla)
+                classVarDec.Add(new XElement(tokenizer.token().type.ToString(), tokenizer.token().content));
+            else
+                throw new CompilerException();
+            tokenizer.advance();
+            if (tokenizer.token().type == JackTokenizer.Token.Type.IDENTIFIER) 
+                classVarDec.Add(new XElement(tokenizer.token().type.ToString(), tokenizer.token().content));
+            else
+                throw new CompilerException();
+            tokenizer.advance();
+            if (tokenizer.token().type == JackTokenizer.Token.Type.SYMBOL) 
+                classVarDec.Add(new XElement(tokenizer.token().type.ToString(), tokenizer.token().content));
+            else
+                throw new CompilerException();
+            classVarDec.WriteTo(writer);
+
+            //XElement child2 = new XElement("AnotherChild",
+            //    new XElement("GrandChild", "different content")
+            //);
+            //child2.WriteTo(writer);
+        }
+    }
+}
--- JackCompiler/JackCompiler.cs
+++ JackCompiler/JackCompiler.cs
@@ -11,6 +11,12 @@ namespace JackCompiler
     {
         static void Main(string[] args)
         {
+            new CompilationEngine("main.jack");
+            //RunJackTokenizer();
+        }
+
+        public static void RunJackTokenizer()
+        {
             JackTokenizer tknz = new JackTokenizer("main.jack");
 
             var settings = new XmlWriterSettings()
@@ -30,125 +36,7 @@ namespace JackCompiler
                 writer.WriteEndElement();
                 writer.Flush();
             }
-
         }
-    }
-    public class JackTokenizer
-    {
-        string JackContent { get; set; }
-        string ActualToken { get; set; }
-        int LineCompiling = 0;
-
-        Queue<string> JackTokensInLine = new Queue<string>();
-        string[] JackLines { get; set; }
-        public JackTokenizer(string fname) {
-            try
-            {
-                using (var sr = new StreamReader(fname))
-                {
-                    JackContent = sr.ReadToEnd();
-                    JackLines = JackContent.Split(
-                        new[] { "\r\n", "\r", "\n" },
-                        StringSplitOptions.None
-                    );
-                    for (int i = 0; i < JackLines.Length; i++)
-                    {
-                        JackLines[i] = Regex.Replace(JackLines[i], @"(//.*)|(/\*(.|\n)*?\*/)", " ").Trim();
-
-                    }                    
-                }
-            }
-            catch (IOException e)
-            {
-                JackContent = "";
-            }
-        }
-        public void advance() {
-
-            if (!(JackTokensInLine.Count > 0))
-            {
-                //verifica Linhas em branco
-                if (JackLines[LineCompiling] == "")
-                {
-                    LineCompiling++;
-                    advance();
-                }
-                else
-                {
-                    foreach (Match Token in Regex.Matches( JackLines[LineCompiling], @"("".*"")|[a-zA-Z_]+[a-zA-Z0-9_]*|[0-9]+|[+|*|/|\-|{|}|(|)|\[|\]|\.|,|;|<|>|=|~]"))
-                    {
-                        JackTokensInLine.Enqueue(Token.Value);
-                    }
-                    LineCompiling++;
-                    ActualToken = JackTokensInLine.Dequeue();
-                }
-            } 
-            else
-                ActualToken = JackTokensInLine.Dequeue();
-        }
-        public bool hasMoreTokens() {
-            if (  LineCompiling < JackLines.Length)
-                return true;
-            else
-                return false;        
-        }
-        public Token token() {
-            return new Token(ActualToken);
-        }
-
-        public class Token {
-            public enum Type
-            {
-                UNIDENTIFIED_TOKEN,
-                KEYWORD,
-                IDENTIFIER,
-                INTEGER_CONSTANT,
-                STRING_CONSTANT,
-                SYMBOL,
-            }
-
-            public Type type;
-
-            public string content;
-            public Token(string token) {
-                content = token;
-                //type = "unkown";
-                if (token != null)
-                {
-                    if (type == 0) findKeyword(token);
-                    if (type == 0) findStringVal(token);
-                    if (type == 0) findIdentifier(token);
-                    if (type == 0) findIntVal(token);
-                    if (type == 0) fundSymbol(token);
-                    if (type == 0) type = Type.UNIDENTIFIED_TOKEN;
-                }
-            }
-            public void findKeyword(string token) {
-                string keywords = "class|constructor|function|"+
-                                  "method|field|static|var|int|"+
-                                  "char|boolean|void|true|false|"+
-                                  "null|this|let|do|if|else|"+
-                                  "while|return";
-                if (Regex.IsMatch(token, keywords)) type = Type.KEYWORD;                
-            }
-            public void fundSymbol(string token) {
-                string symbols = @"^({|}|(|)|\[|\]|.|,|;|\+|-|\*|\/|&||<|>|=|~)$";
-                if (Regex.IsMatch(token, symbols)) type = Type.SYMBOL;
-            }
-            public void findIdentifier(string token)
-            {
-                string identifier = "[a-zA-Z_]+[a-zA-Z0-9_]*";
-                if (Regex.IsMatch(token, identifier)) type = Type.IDENTIFIER;
-            }
-            public void findIntVal(string token) {
-                string integer = "[0-9]+";
-                if (Regex.IsMatch(token, integer)) type = Type.INTEGER_CONSTANT;
-            }
-            public void findStringVal(string token) {
-                if (Regex.IsMatch(token, "(\".*\")")) type = Type.STRING_CONSTANT;
-            }
-        }
-
     }
 
 }
